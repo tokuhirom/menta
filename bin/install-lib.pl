d297e16133699e604107bb4f2f11fe9ba22fb3c8
tokuhirom <tokuhirom@d0d07461-0603-4401-acd4-de1884942a52>
2008-11-18 06:07:42 +0000

    特定のディレクトリにモジュールを一発で入れられるスクリプト
    git-svn-id: http://svn.coderepos.org/share/lang/perl/MENTA/trunk@24084 d0d07461-0603-4401-acd4-de1884942a52

---
+++ bin/install-lib.pl
@@ -0,0 +1,73 @@
+use strict;
+use warnings;
+use CPAN;
+use File::Temp qw/tempdir tempfile/;
+use File::Spec::Functions;
+use FindBin;
+use Cwd;
+use Data::Dumper;
+use Module::CoreList;
+use Getopt::Long;
+
+my %installed;
+my %optional_args = (
+    'version' => '--perl_only',
+);
+my $target_version = '5.008001';
+my $outdir;
+
+&main; exit;
+
+# utils
+sub Path::Class::Dir::basename { shift->dir_list(-1, 1) }
+
+sub main {
+    # process args
+    GetOptions(
+        "version=f", \$target_version,
+    );
+    unless (@ARGV == 2) {
+        die "Usage: $0 Acme::Hello extlib/";
+    }
+    my ($pkg, $dstdir) = @ARGV;
+
+    # init
+    my $cwd = getcwd();
+    my $tmpdir = tempdir(CLENAUP => 1);
+    $outdir = catfile($tmpdir, "outputdir");
+    mkdir -d $outdir;
+    CPAN::HandleConfig->load;
+    CPAN::Shell::setup_output;
+
+    # install
+    install_pkg($pkg);
+
+    chdir $cwd;
+
+    # copy to dst dir
+    my $outlibdir = catfile($outdir, 'lib', 'perl5') . '/';
+    print "sync $outlibdir => $dstdir\n";
+    system qw/rsync --verbose --recursive/,  $outlibdir, $dstdir;
+}
+
+sub install_pkg {
+    my $pkg = shift;
+    return if $installed{$pkg};
+    $installed{$pkg}++;
+    return if $Module::CoreList::version{$target_version}{$pkg};
+
+    local $CPAN::Config->{histfile}   = tempfile(CLEANUP => 1);
+    local $CPAN::Config->{makepl_arg} = "INSTALL_BASE=$outdir " . ($optional_args{$pkg} ? $optional_args{$pkg} : '');
+    local $CPAN::Config->{mbuildpl_arg} = "--install_base=$outdir";
+
+    my $mod = CPAN::Shell->expand("Module", $pkg) or die "cannot find $pkg";
+    my $dist = $mod->distribution;
+    $mod->distribution->make;
+    if (my $requires = $dist->prereq_pm) {
+        for my $req (keys %{$requires->{requires}}) {
+            install_pkg($req);
+        }
+    }
+    $dist->install();
+}
+
