638d4e5d9947ea147af1b49ef68fffcca66d4238
mattn <mattn@d0d07461-0603-4401-acd4-de1884942a52>
2008-11-20 03:19:42 +0000

    get base directory from data_dir.
    git-svn-id: http://svn.coderepos.org/share/lang/perl/MENTA/trunk@24419 d0d07461-0603-4401-acd4-de1884942a52

--- app/controller/users.pl
+++ app/controller/users.pl
@@ -3,7 +3,7 @@ use MENTA;
 load_plugin("sql");
 
 sub do_users {
-    sql_dbh('DBI:CSV:f_dir=app/data');
+    sql_dbh('DBI:CSV:f_dir='.data_dir);
     my $rows = sql_select_all('select * from users');
     render('users.mt', $rows);
 }
