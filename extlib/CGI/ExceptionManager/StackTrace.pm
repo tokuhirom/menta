4cc6cd5f88e897a9f33f7533b324d1a73ba04fe0
tokuhirom <tokuhirom@d0d07461-0603-4401-acd4-de1884942a52>
2008-12-18 13:42:17 +0000

    we need pre. because T::MT wrote \n in error message
    git-svn-id: http://svn.coderepos.org/share/lang/perl/MENTA/trunk@27024 d0d07461-0603-4401-acd4-de1884942a52

--- extlib/CGI/ExceptionManager/StackTrace.pm
+++ extlib/CGI/ExceptionManager/StackTrace.pm
@@ -74,7 +74,7 @@ sub _build_context {
 sub as_html {
     my ($err, %args) = @_;
     my $msg = _escape_html($err->{message});
-    my $out = qq{<!doctype html><head><title>500 Internal Server Error</title><style type="text/css">body { margin: 0; padding: 0; background: rgb(230, 230, 230); color: rgb(44, 44, 44); } h1 { margin: 0 0 .5em; padding: .25em .5em .1em 1.5em; border-bottom: thick solid rgb(0, 0, 15); background: rgb(63, 63, 63); color: rgb(239, 239, 239); font-size: x-large; } p { margin: .5em 1em; } li { font-size: small; } pre { background: rgb(255, 239, 239); color: rgb(47, 47, 47); font-size: medium; } pre code strong { color: rgb(0, 0, 0); background: rgb(255, 143, 143); } p.f { text-align: right; font-size: xx-small; } p.f span { font-size: medium; }</style></head><h1>500 Internal Server Error</h1><p>${msg}</p><ol>};
+    my $out = qq{<!doctype html><head><title>500 Internal Server Error</title><style type="text/css">body { margin: 0; padding: 0; background: rgb(230, 230, 230); color: rgb(44, 44, 44); } h1 { margin: 0 0 .5em; padding: .25em .5em .1em 1.5em; border-bottom: thick solid rgb(0, 0, 15); background: rgb(63, 63, 63); color: rgb(239, 239, 239); font-size: x-large; } p { margin: .5em 1em; } li { font-size: small; } pre { background: rgb(255, 239, 239); color: rgb(47, 47, 47); font-size: medium; } pre code strong { color: rgb(0, 0, 0); background: rgb(255, 143, 143); } p.f { text-align: right; font-size: xx-small; } p.f span { font-size: medium; }</style></head><h1>500 Internal Server Error</h1><pre>${msg}</pre><ol>};
     for my $stack (@{$err->{trace}}) {
         $out .= join(
             '',
