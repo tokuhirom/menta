1b9520b0a5de12c900904da1933fa989ec6295fa
Tokuhiro Matsuno <tokuhirom@gmail.com>
2009-12-02 18:56:17 +0900

    環境かわったんで、deploy script の設定かえる。

--- author/deploy.sh
+++ author/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
+git push --tags
 sudo -u www-data rm -rf /var/www/menta/
 sudo -u www-data mkdir /var/www/menta/
 sudo -u www-data cp -a * /var/www/menta/
-sudo /etc/init.d/apache2 restart
