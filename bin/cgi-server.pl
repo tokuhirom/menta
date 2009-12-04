05784faf3120042adc5d16aec94a043582a1cab2
Tokuhiro Matsuno <tokuhirom@gmail.com>
2009-12-04 23:53:41 +0900

    cgi-server.pl を reloadable にした。

--- bin/cgi-server.pl
+++ bin/cgi-server.pl
@@ -5,14 +5,31 @@ use lib 'extlib', 'lib';
 use Getopt::Long;
 use Plack;
 use Plack::Server::Standalone;
+use Plack::Loader::Reloadable;
+use Plack::Middleware::StackTrace;
+use Plack::Middleware::AccessLog;
+use Plack::Builder;
+use Plack::Util;
+use Plack::Loader;
 
 my $port = 5555;
 GetOptions(
     'p|port=i' => \$port,
 );
 
-my $app = do 'menta.psgi';
+# delay the build process for reloader
+sub build(&;$) {
+    my $block = shift;
+    my $app   = shift || sub { };
+    return sub { $block->($app->()) };
+}
 
-my $server = Plack::Server::Standalone->new(port => $port);
+my $psgi = 'menta.psgi';
+my $handler = build { Plack::Util::load_psgi $psgi };
+my $loader = Plack::Loader::Reloadable->new(['.']);
+$handler = build { Plack::Middleware::StackTrace->wrap($_[0]) } $handler;
+$handler = build { Plack::Middleware::AccessLog->wrap($_[0], logger => sub { print STDERR @_ }) } $handler;
+my $app = $handler;
+my $server = $loader->load('Standalone', port => $port);
 $server->run($app);
 
