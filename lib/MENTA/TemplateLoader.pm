5c0f9facee57d4d35a3336b83b397443daeb8e12
Tokuhiro Matsuno <tokuhirom@gmail.com>
2009-12-05 13:55:38 +0900

    T::MT がかわったせいか、なぜか引数の処理がうまくいかなくなってたので、T::MT::File を普通につかうようにした。

--- lib/MENTA/TemplateLoader.pm
+++ lib/MENTA/TemplateLoader.pm
@@ -8,64 +8,14 @@ use utf8;
 sub __load {
     my ($path, @params) = @_;
 
-    if (__use_cache($path)) {
-        my $tmplfname = MENTA::mt_cache_dir . "/$path.c";
-        local $@;
-        my $tmplcode = do $tmplfname;
-        die $@ if $@;
-        die "テンプレートキャッシュを読み込めませんでした: ${tmplfname}($!)" unless $tmplcode;
-        return $tmplcode->(@params)->as_string;
-    } else {
-        return __compile($path, @params);
-    }
-}
-
-sub __compile {
-    my ($path, @params) = @_;
     MENTA::Util::require_once('Text/MicroTemplate/File.pm');
 
     my $mtf = Text::MicroTemplate::File->new(
         include_path => [MENTA::controller_dir()],
         package_name => 'MENTA::TemplateLoader::Instance',
+        cache        => 1,
     );
-    my $out = $mtf->build_file($path)->(@params)->as_string;
-    __update_cache($path, $mtf->code);
-    return $out;
-}
-
-sub __update_cache {
-    my ($path, $code) = @_;
-
-    $code = <<"...";
-package MENTA::TemplateLoader::Instance;
-sub {
-    local \$SIG{__WARN__} = sub { print STDERR \$_mt->_error(shift, 4, \$_from) };
-    Text::MicroTemplate::encoded_string((
-        $code
-    )->(\@_));
-}
-...
-
-    my $cache_path = MENTA::mt_cache_dir;
-    foreach my $p (split '/', $path) {
-        mkdir $cache_path;
-        $cache_path .= "/$p";
-    }
-    $cache_path .= '.c';
-    open my $fh, '>:utf8', $cache_path
-        or die "キャッシュファイルを作れません: $cache_path($!)";
-    print $fh $code;
-    close $fh;
-}
-
-sub __use_cache {
-    my ($path) = @_;
-    return unless MENTA::mt_cache_dir;
-    my @orig = stat $path
-        or return;
-    my @cached = stat MENTA::mt_cache_dir . "/${path}.c"
-        or return;
-    return $orig[9] < $cached[9];
+    return $mtf->render_file($path, @params)->as_string;
 }
 
 {
@@ -77,4 +27,3 @@ sub __use_cache {
 }
 
 "ENDOFMODULE";
-
