a0d4f2044bceeb51179c43e6931f730d8a00d2d7
Tokuhiro Matsuno <tokuhirom@gmail.com>
2009-12-06 13:44:08 +0900

    キャッシュ機能を再度つくりこむ。work in progress

--- lib/MENTA/TemplateLoader.pm
+++ lib/MENTA/TemplateLoader.pm
@@ -2,21 +2,104 @@ package MENTA::TemplateLoader;
 use strict;
 use warnings;
 use utf8;
+use Carp ();
 
 # originaly code from NanoA::TemplateLoader
 
 sub __load {
     my ($path, @params) = @_;
 
-    MENTA::Util::require_once('Text/MicroTemplate/Extended.pm');
+    if (__use_cache($path)) {
+        my $tmplfname = MENTA::mt_cache_dir . "/$path.c";
+        local $@;
+        my $tmplcode = do $tmplfname;
+        die $@ if $@;
+        die "テンプレートキャッシュを読み込めませんでした: ${tmplfname}($!)" unless $tmplcode;
+        return $tmplcode->(@params)->as_string;
+    } else {
+        return __compile($path, @params);
+    }
+}
 
-    my $mtf = Text::MicroTemplate::Extended->new(
-        include_path => [MENTA::controller_dir()],
+sub __compile {
+    my ($path, @params) = @_;
+    MENTA::Util::require_once('Text/MicroTemplate.pm');
+
+    my $mt = Text::MicroTemplate->new(
         package_name => 'MENTA::TemplateLoader::Instance',
-        extension    => '',
-        cache        => 1,
     );
-    return $mtf->render_file($path, @params)->as_string;
+    __build_file($mt, $path);
+    my $code = __eval_builder($mt->code);
+    my $compiled = do {
+        local $SIG{__WARN__} = sub {
+            print STDERR $mt->_error(shift, 4, $MENTA::TemplateLoader::Instance::render_context->{caller});
+        };
+
+        my $ret = eval $code;
+        die "テンプレートコンパイルエラー\n$@" if $@;
+        $ret;
+    };
+    my $out = $compiled->($mt, @params);
+    __update_cache($path, $code);
+    return $out;
+}
+
+sub __build_file {
+    my ($mt, $file) = @_;
+    my $path = MENTA::controller_dir;
+    my $filepath = $path . '/' . $file;
+
+    open my $fh, "<:utf8", $filepath
+        or Carp::croak("テンプレートファイルがひらけません :$filepath:$!");
+    my $src = do { local $/; <$fh> };
+    close $fh;
+
+    $mt->parse($src);
+}
+
+sub __eval_builder {
+    my $code = shift;
+    return <<"...";
+package MENTA::TemplateLoader::Instance;
+#line 1
+sub {
+    my \$_mt = shift;
+    local \$MENTA::TemplateLoader::Instance::render_context = {};
+    my \$out = Text::MicroTemplate::encoded_string((
+        $code
+    )->(\@_));
+    if (my \$parent = \$MENTA::TemplateLoader::Instance::render_context->{extends}) {
+        \$out = MENTA::TemplateLoader::__load(\$parent);
+    }
+    \$out;
+}
+...
+}
+
+sub __update_cache {
+    my ($path, $code) = @_;
+
+    my $cache_path = MENTA::mt_cache_dir;
+    foreach my $p (split '/', $path) {
+        mkdir $cache_path;
+        $cache_path .= "/$p";
+    }
+    $cache_path .= '.c';
+    open my $fh, '>:utf8', $cache_path
+        or die "キャッシュファイルを作れません: $cache_path($!)";
+    print $fh $code;
+    close $fh;
+}
+
+sub __use_cache {
+    my ($path) = @_;
+    return if $ENV{DEBUG};
+    return unless MENTA::mt_cache_dir;
+    my @orig = stat $path
+        or return;
+    my @cached = stat MENTA::mt_cache_dir . "/${path}.c"
+        or return;
+    return $orig[9] < $cached[9];
 }
 
 {
@@ -24,6 +107,41 @@ sub __load {
     no strict 'refs';
     for my $meth (qw/escape_html unescape_html raw_string config render param mobile_agent uri_for static_file_path docroot AUTOLOAD redirect current_url/) {
         *{__PACKAGE__ . '::' . $meth} = *{"MENTA::$meth"};
+    }
+
+    # following code is taken from Text::MicroTemplate::Extended by typester++.
+    our $render_context;
+
+    sub extends {
+        $render_context->{extends} = $_[0];
+    }
+
+    sub block {
+        my ($name, $code) = @_;
+
+        my $block;
+        if (defined $code) {
+            $block = $render_context->{blocks}{$name} ||= {
+                context_ref => "MENTA::TemplateLoader::Instance::_MTREF",
+                code        => ref($code) eq 'CODE' ? $code : sub { return $code },
+            };
+        }
+        else {
+            $block = $render_context->{blocks}{$name}
+                or die qq[block "$name" does not define];
+        }
+
+        if (!$render_context->{extends}) { # if base template.
+            my $current_ref = $MENTA::TemplateLoader::Instance::_MTREF;
+            my $block_ref   = $block->{context_ref};
+
+            my $rendered = $$current_ref || '';
+            $$block_ref = '';
+
+            my $result = $block->{code}->() || $$block_ref || '';
+
+            $$current_ref = $rendered . $result;
+        }
     }
 }
 
