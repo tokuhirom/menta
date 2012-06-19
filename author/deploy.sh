63861e818e3104edca75b29907bbde4c266b206e
tokuhirom <tokuhirom@gmail.com>
2012-06-19 13:28:03 +0900

    deploy to dev2.64p.org

--- author/deploy.sh
+++ author/deploy.sh
@@ -1,4 +1,20 @@
-#!/bin/sh
-sudo -u www-data rm -rf /var/www/menta/
-sudo -u www-data mkdir /var/www/menta/
-sudo -u www-data cp -a * /var/www/menta/
+#!/usr/bin/zsh
+DOMAIN=menta.64p.org
+
+if [ ! -f /etc/init/$DOMAIN.conf ]; then
+    sudo ln -s `pwd`/author/etc/init/$DOMAIN.conf /etc/init/$DOMAIN.conf
+fi
+
+if [ ! -f /etc/nginx/sites-enabled/$DOMAIN.conf ]; then
+    sudo ln -s `pwd`/author/etc/httpd/$DOMAIN.conf /etc/nginx/sites-enabled/$DOMAIN.conf
+fi
+
+sudo /etc/init.d/nginx reload
+sudo initctl reload-configuration
+sudo stop $DOMAIN
+sudo start $DOMAIN
+
+echo "--------------------------"
+echo "Deployment finished"
+sudo tail /var/log/upstart/$DOMAIN.log
+
